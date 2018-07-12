c2103b04116558acfa938d186d42fea109566fee
Slushy <ess090502@gmail.com>
2018-07-12 16:39:19 -0500

    begin working on autofill function and added case if no autofills found

--- autofill.py
+++ autofill.py
@@ -3,7 +3,7 @@ import codecs
 
 #classes
 class TrieNode:
-    def __init__(self, value: str):
+    def __init__(self, value):
         self.value = value
         self.count = 1
         self.children = []
@@ -17,11 +17,42 @@ def add(root, word):
                 child.count+=1#increase count by one
                 node = child#advance down the node root as the character advance through the word
                 inChild = True#was in child so skips if statement
+                break#once found no need to continue
         if(not inChild):#if it is not in child outside the child in children loop because it runs 0 times with no children
             new_node = TrieNode(char)#make new node with the value
             node.children.append(new_node)#add it to children
             node = new_node#go down the node root as characters advance
 
+def checkInTrie(UInput, trie):
+    node = trie
+    existsInChild = False
+    for char in UInput:
+        for child in node.children:
+            if char == child.value:
+                node = child#move down the line to continue moving down the word
+                existsInChild=True
+                break#no longer need to continue this loop
+            else:
+                existsInChild = False
+        if(not existsInChild):
+            return False
+    return True#if every word exists in child then return true
+
+def advanceToEndOfWord(UInput, trie):
+    node = trie
+    for char in UInput:
+        for child in node.children:
+            if char == child.value:
+                node = child
+                break
+    return node
+
+def returnCompletions(UInput, trie):
+    node = advanceToEndOfWord(UInput,trie)#move down trie until end of word fragment
+    possibleNumberOfAutofills=len(node.children)#how many possible autofills
+    #sort children from highest inputs into them highest to lowest
+    #return up to top 5 possible autofill endings to it
+    #end function by returning the array of autofills
 
 #variables
 historyArray = codecs.open("my_history.txt").read().lower().split("\n")#split the words into an array
@@ -30,4 +61,10 @@ trie = TrieNode("*")#root node
 #start programming
 for word in historyArray:#setting up nodes
     add(trie, word)#add new nodes taking root and word. Because of how arrays work no need to return it as it indexes to the same array)
-notFullInput = input("what would you like to autofill. Put in a word fraction and see if it autofills.\n")
+notFullInput = input("What would you like to autofill? Put in a word fraction and see if it autofills.\n")
+if(checkInTrie(notFullInput, trie)):
+    autofillResults = returnCompletions(notFullInput,trie)
+    for result in autofillResults:
+        print(result)
+else:
+    print("No results were found.")
